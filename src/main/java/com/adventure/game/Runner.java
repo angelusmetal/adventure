ad7b7d116a0e274952ca2db9ab366378ae22544d
Rodrigo Fernandez <angelusmetal@gmail.com>
2013-06-18 23:05:18 -0300

    Updated runner so that it picks the fiction file from a command line argument.

--- src/main/java/com/adventure/game/Runner.java
+++ src/main/java/com/adventure/game/Runner.java
@@ -13,7 +13,6 @@ import com.adventure.engine.console.Vocabulary;
 import com.adventure.engine.entity.Entity;
 import com.adventure.engine.script.GameContextReader;
 import com.adventure.engine.script.expression.ExpressionParserException;
-import com.adventure.engine.script.expression.VocabularyParser;
 import com.adventure.engine.script.syntax.SyntaxParser.ScriptParsingException;
 
 public class Runner {
@@ -29,67 +28,48 @@ public class Runner {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Runner runner = new Runner();
+		if (args.length != 1) {
+			System.err.println("You should provide a fiction file.");
+			System.exit(1);
+		}
+		String filename = args[0];
+		Runner runner = new Runner(filename);
 		runner.mainLoop();
 	}
 	
-	public Runner() {
+	public Runner(String filename) {
 		try {
-			readScript();
-			initParser();
+			readScript(filename);
 		} catch (ExpressionParserException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (ScriptParsingException e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
 	}
 	
-	private void initParser() throws ExpressionParserException {
+	private void readScript(String filename) throws ExpressionParserException, FileNotFoundException, IOException, ScriptParsingException {
+		GameContextReader reader = new GameContextReader();
+		context = reader.readFrom(new FileInputStream(filename));
 		
-//		// Get verbs from the start object
-//		VocabularyParser verbsParser = new VocabularyParser();
-//		Entity voc = context.getEntity("vocabulary");
-//		
-//		// Parse them
-//		Vocabulary verbs = verbsParser.parse(start.getProperty("verbs"));
-		
-		// Make the interested parties aware of them
+		// Configure parser
 		parser.setVocabulary(context.getVocabulary());
-		
-//		// Configure articles
-//		List<String> articles = start.getProperty("articles").getValue().getAsList();
-//		parser.setArticles(articles);
-//		
-//		// Configure prepositions
-//		List<String> prepositions = start.getProperty("prepositions").getValue().getAsList();
-//		parser.setPrepositions(prepositions);
-//
-	}
-	
-	private void readScript() throws ExpressionParserException {
-		try {
-			GameContextReader reader = new GameContextReader();
-			context = reader.readFrom(new FileInputStream("sample.fiction"));
-			
-			start = context.getEntity("@start");
-			
-			// Set starting location
-			String startingLocation = start.getProperty("location").getValue().getAsString();
-			context.setCurrentLocation(context.getEntity(startingLocation));
 
-			String welcome = start.getProperty("welcome").getValue().getAsString();
-			context.display(welcome);
-			
-//			System.out.println(context.showContent());
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ScriptParsingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		// Start configuration
+		start = context.getEntity("@start");
+		
+		String startingLocation = start.getProperty("location").getValue().getAsString();
+		context.setCurrentLocation(context.getEntity(startingLocation));
+
+		String welcome = start.getProperty("welcome").getValue().getAsString();
+		context.display(welcome);
 	}
 	
 	private void mainLoop() {
